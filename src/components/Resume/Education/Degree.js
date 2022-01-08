f86d24a1d6beae70996e3d71b42584e6ca5334f2
Yi Zhang <cis520@plus.seas.upenn.edu>
2022-01-08 01:24:30 -0500

    update

--- src/components/Resume/Education/Degree.js
+++ src/components/Resume/Education/Degree.js
@@ -5,9 +5,9 @@ const Degree = ({ data }) => (
   <article className="degree-container">
     <header>
       <h4 className="degree">{data.degree}</h4>
-      <h5 className="school"><a href={data.link}>{data.school}</a>, {data.year}</h5>
-      <h4 className="school">Thesis Advisors: Zachary G. Ives, Dan Roth.</h4>
-      <h4 className="school">Thesis Committee: Fernando Pereira, Chris Callison-burch, Andrew Head, Boon Thau Loo.</h4>
+      <h4 className="school"><a href={data.link}>{data.school}</a>, {data.year}</h4>
+      <h5 className="school">Thesis Advisors: Zachary G. Ives, Dan Roth.</h5>
+      <h5 className="school">Thesis Committee: Fernando Pereira (Google), Chris Callison-burch, Andrew Head, Boon Thau Loo.</h5>
     </header>
   </article>
 );
