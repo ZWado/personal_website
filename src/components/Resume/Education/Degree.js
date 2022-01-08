1ec5fa5c6bf9bece1891a0b70281bd3afde64b51
Yi Zhang <cis520@plus.seas.upenn.edu>
2022-01-08 01:27:01 -0500

    update

--- src/components/Resume/Education/Degree.js
+++ src/components/Resume/Education/Degree.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types';
 const Degree = ({ data }) => (
   <article className="degree-container">
     <header>
-      <h4 className="degree">{data.degree}</h4>
+      <h5 className="degree">{data.degree}</h5>
       <h4 className="school"><a href={data.link}>{data.school}</a>, {data.year}</h4>
-      <h5 className="school">Thesis Advisors: Zachary G. Ives, Dan Roth.</h5>
-      <h5 className="school">Thesis Committee: Fernando Pereira (Google), Chris Callison-burch, Andrew Head, Boon Thau Loo.</h5>
+      <h4 className="school">Thesis Advisors: Zachary G. Ives, Dan Roth.</h4>
+      <h4 className="school">Thesis Committee: Fernando Pereira (Google), Chris Callison-burch, Andrew Head, Boon Thau Loo.</h4>
     </header>
   </article>
 );
